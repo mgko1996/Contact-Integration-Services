4191ed41cec0659adf4d681079f123d34e782f72
myeonggi ko <myeonggi_ko@tmax.co.kr>
2023-04-18 13:20:20 +0900

    start: spring project setting

---
+++ backend/src/main/java/site/conthub/conthub/ConthubApplication.java
@@ -0,0 +1,13 @@
+package site.conthub.conthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConthubApplication.class, args);
+	}
+
+}
